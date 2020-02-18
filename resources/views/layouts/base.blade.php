7315c7c9c870dcd308046e2e5df63c5b46a2e9ea
= <=>
2020-02-18 21:54:31 +1300

    Changed colours

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -59,6 +59,7 @@
             </div><!--//container-->
         </header><!--//header-->
         <nav class="navbar navbar-expand-lg navbar-light style="background-color: ">
+            <p class="navbar-brand" style="color: white;">..........<p>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
@@ -84,7 +85,7 @@
                     Admin
                   </a>
                   <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="{{ route('voyager.login') }}">Admin Panel</a>
+                    <a class="dropdown-item" href="/admin/login">Admin Panel</a>
                     <a class="dropdown-item" href="{{ route('manuscripts.create') }}">Create New Manuscript</a>
                     <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Logout</a>
                     <form action="{{ route('logout') }}" method="post" id="logout-form" style="display: none;">
